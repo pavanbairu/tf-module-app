17a0f8947ab2ba2be4c23baf988df011384f0066
pavanbairu <pavan.bairu1@gmail.com>
2023-06-03 12:30:08 +0530

    created docdb cluster and  parameter group

--- main.tf
+++ main.tf
@@ -26,29 +26,24 @@ resource "aws_security_group" "sg" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
-  tags = {
-    Name = "${var.name}-${var.env}-sg"
-  }
+  tags = merge(var.tags, { Name = "${var.name}-${var.env}-sg" })
 }
 resource "aws_launch_template" "template" {
-  name_prefix   = "${var.name}-${var.env}-lt"
-  image_id      = data.aws_ami.ami.id
-  instance_type = var.instance_type
+  name_prefix            = "${var.name}-${var.env}-lt"
+  image_id               = data.aws_ami.ami.id
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.sg.id]
 }
-
 resource "aws_autoscaling_group" "asg" {
-  name   = "${var.name}-${var.env}-asg"
-  desired_capacity   = var.desired_capacity
-  max_size           = var.max_size
-  min_size           = var.min_size
+  name                = "${var.name}-${var.env}-asg"
+  desired_capacity    = var.desired_capacity
+  max_size            = var.max_size
+  min_size            = var.min_size
   vpc_zone_identifier = var.subnet_ids
-
   launch_template {
     id      = aws_launch_template.template.id
     version = "$Latest"
   }
-
   dynamic "tag" {
     for_each = local.asg_tags
     content {
