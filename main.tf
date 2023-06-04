198b23d8a94eee75aa207983882eae4b4c11ec54
pavanbairu <pavan.bairu1@gmail.com>
2023-06-04 11:35:29 +0530

    create load balancer target group and attached it under corresponding app autoscaling scaling group

--- main.tf
+++ main.tf
@@ -40,6 +40,8 @@ resource "aws_autoscaling_group" "asg" {
   max_size            = var.max_size
   min_size            = var.min_size
   vpc_zone_identifier = var.subnet_ids
+  target_group_arns   = [aws_lb_target_group.main.arn]
+
   launch_template {
     id      = aws_launch_template.template.id
     version = "$Latest"
@@ -53,3 +55,12 @@ resource "aws_autoscaling_group" "asg" {
     }
   }
 }
+
+resource "aws_lb_target_group" "main" {
+  name     = "${var.name}-${var.env}-tg"
+  port     = var.app_port
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  tags     = merge(var.tags, { Name = "${var.name}-${var.env}-tg" })
+}
+
